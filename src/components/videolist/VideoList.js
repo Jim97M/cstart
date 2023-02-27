ee4a6dc3c9573cf8f577bdd47b4d593d7a54f312
Jim97M <wabuyajames@gmail.com>
2023-02-27 22:36:31 +0300

    Video List Fetch Styling and Styled Video Page

--- src/components/videolist/VideoList.js
+++ src/components/videolist/VideoList.js
@@ -1,7 +1,7 @@
+import './VideoList.scss';
 import React, {useState, useEffect} from 'react';
-import { DefaultPlayer as Video } from 'react-html5video/dist';
-import 'react-html5video/dist/styles.css';
 import ReactPlayer from "react-player";
+import {FaVideo} from 'react-icons/fa';
 import axios from 'axios';
 
 
@@ -21,14 +21,25 @@ const VideoList = () => {
   return (
     <div>
     {video.map((file) => (
+         <div style={{marginLeft: 20, marginTop: 50, height: 500, width: 600, boxShadow: "0 0 3px 2px #cec7c759", padding: 20,  borderRadius: 20, flex: 1, flexDirection: 'column'}}>
          <ReactPlayer
          className='react-player fixed-bottom'
          url= {`${file.name}`}
-         width='50%'
-         height='50%'
+         width='100%'
+         height='100%'
+         style={{marginTop: -50}}
          controls = {true}
-        
          />
+         <div style={{display:'flex', marginLeft: 10}}>
+         <div style={{ marginTop: 10, height: 40, width: 130, boxShadow: "0 0 3px 2px #cec7c759", display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+           <h5> {file.tutorial_name}  </h5> <FaVideo size={30} color='blue' style={{marginLeft: 5}}  />
+          
+         </div>
+          <p style={{marginLeft: 50}}>
+            {file.tutorial_description}
+          </p>
+         </div>
+        </div>
       ))}
     </div>
   )
